cbe2062efc537981ef6d4cbebc5a011a012d097c
Rahul Yadav <yadavrahul776393@gmail.com>
2026-05-24 00:11:05 +0530

    Fixed views path

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 const Offer = require("./models/offer");
 const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride("_method"));
